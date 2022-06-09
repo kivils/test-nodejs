c15adf618a2fc0e0d60ab111beeffdc17e4f2a9a
Yulia Sesyunina <idwebkivils@gmail.com>
2022-06-09 13:04:20 +0200

    Section 5: Express.js: Midway Assessment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+const userForm =
+  '<form action="/create-user" method="POST">' +
+  '<label for="username">Pick a username</label><br/>' +
+  '<input type="text" name="username" id="username"/>' +
+  '<button type="submit">Join us!</button>' +
+  '</form>'
+
 app.use('/users', (req, res) => {
-  console.log('In User middleware');
   res.send(
     '<p><a href="/">Our home page</a></p>' +
     '<h1>Our people</h1>' +
@@ -14,31 +23,30 @@ app.use('/users', (req, res) => {
     '<li>Federico Fellini</p>' +
     '</ol>' +
     '<h2>Please join us :-)</h2>' +
-    '<form action="/create-user" method="POST">' +
-    '<label for="username">Pick a username</label><br/>' +
-    '<input type="text" name="username" id="username"/>' +
-    '<button type="submit">Join us!</button>' +
-    '</form>'
+    userForm
   );
 })
 
-app.use('/create-user', (req, res) => {
-  console.log('In Create User middleware');
-  res.send('<h1>Welcome to the Club!</h1>');
+app.post('/create-user', (req, res) => {
+  console.log(req.body);
+  res.send('' +
+    '<p><a href="/">Our home page</a></p>' +
+    '<h1>Wow <span style="color: green;">' + req.body.username + '</span>! Welcome to the club :-) !</h1>' +
+    '<p>Now you are <a href="/users">one of us</a>!</p>'
+  );
+})
+
+app.get('/create-user', (req, res) => {
+  res.redirect('/');
 })
 
 // Should go in the end after all other use() calls
 app.use('/', (req, res) => {
-  console.log('In Default middleware');
   res.send(
     '<h1>Hey, nice to see you!</h1>' +
     '<p>We have many greate people here, <a href="/users">just have a look</a>!</p>' +
     '<h2>Please join us :-)</h2>' +
-    '<form action="/create-user" method="POST">' +
-    '<label for="username">Pick a username</label><br/>' +
-    '<input type="text" name="username" id="username"/>' +
-    '<button type="submit">Join us!</button>' +
-    '</form>'
+    userForm
   );
 })
 
