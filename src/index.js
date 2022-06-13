b41417e00deb62ee45f8fbb5815e48644e485698
Yulia Sesyunina <idwebkivils@gmail.com>
2022-06-13 17:30:23 +0200

    Section 5: Express.js: Added livereload

--- src/index.js
+++ src/index.js
@@ -2,19 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const shopRouter = require('./routes/shop');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
+
+const publicDirectory = path.join(__dirname, '..', 'public');
+
+// For frontend live reload
+// @see https://bytearcher.com/articles/refresh-changes-browser-express-livereload-nodemon/
+// TODO: Check how to get rid of this on production
+const liveReloadServer = livereload.createServer();
+
+liveReloadServer.watch(publicDirectory);
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
+// App
+const defaultRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
 
 const app = express();
 
+// For frontend live reload
+app.use(connectLivereload());
+
 app.use(bodyParser.urlencoded({extended: false}));
 // Define path to static resources
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(publicDirectory));
 
 // Routes started with /users
 app.use('/users', usersRouter);
 
-app.use(shopRouter);
+app.use(defaultRouter);
 
 app.use((req, res, next) => {
   res
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Should go in the end after all other use() calls
 router.get('/', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'index.html'));
 })
 
 module.exports = router;
