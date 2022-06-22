aa628c40965fcb9c867ae9ac6092db8c2119d9be
Yulia Sesyunina <idwebkivils@gmail.com>
2022-06-22 13:00:50 +0200

    Section 7: MVC: Refactired user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,34 +4,33 @@ const rootDir = require('../helpers/path');
 
 const usersData = path.join(rootDir, 'data', 'users.json');
 
+const readUsersFromFile = (cb) => {
+  fs.readFile(usersData, (err, fileContent) => {
+    if(err) {
+      cb([]);
+    }
+    else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+}
+
 module.exports = class User  {
   constructor(name) {
     this.name = name;
   };
 
   save() {
-    fs.readFile(usersData, (err, fileContent) => {
-      let users = [];
-      if(!err) {
-        users = JSON.parse(fileContent);
-      }
-
+    readUsersFromFile(users => {
       users.push(this);
 
       fs.writeFile(usersData, JSON.stringify(users), err => {
         console.log('Error: ', err);
       });
-    });
+    })
   };
 
   static fetchAll(cb) {
-    fs.readFile(usersData, (err, fileContent) => {
-      if(err) {
-        cb([]);
-      }
-      else {
-        cb(JSON.parse(fileContent));
-      }
-    });
+    readUsersFromFile(cb);
   }
 }
